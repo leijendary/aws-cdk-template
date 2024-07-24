72edcb3883e8eb36219d42b70771772b3be77dff
leijendary <jonathanleijendekker@gmail.com>
2024-07-24 09:45:55 +0200

    [no ci] Updated fetch and used startDate instead of queryDate

--- function/alarm-notifier.ts
+++ function/alarm-notifier.ts
@@ -1,13 +1,13 @@
 import { Handler } from "aws-lambda";
 
 export const handler: Handler = async (event) => {
-  console.log(event);
+  console.log("Event", event);
 
   const alarmName = event["alarmData"]["alarmName"];
   const state = event["alarmData"]["state"];
   const reason = state["reason"];
   const reasonData = JSON.parse(state["reasonData"]);
-  const queryDate = reasonData["queryDate"];
+  const startDate = reasonData["startDate"];
   const timestamp = state["timestamp"];
   const options = {
     method: "POST",
@@ -17,12 +17,16 @@ export const handler: Handler = async (event) => {
     },
     body: JSON.stringify({
       channel: process.env.SLACK_CHANNEL!!,
-      text: `*Alarm*: ${alarmName}.\n*Reason*: ${reason}\n*Timestamp*: Between ${queryDate} and ${timestamp}.`,
+      text: `*Alarm*: ${alarmName}.\n*Reason*: ${reason}\n*Timestamp*: Between ${startDate} and ${timestamp}.`,
     }),
   };
 
-  fetch("https://slack.com/api/chat.postMessage", options)
-    .then((response) => response.text())
-    .then(console.log)
-    .catch(console.error);
+  try {
+    const response = await fetch("https://slack.com/api/chat.postMessage", options);
+    const json = await response.json();
+
+    console.log("Response", json);
+  } catch (error) {
+    console.error(error);
+  }
 };
